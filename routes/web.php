c56dd11fdd91ef261f4a49670de9dc1f0810ae02
Yohei Umezu <umeyouhei@gmail.com>
2019-05-22 10:53:19 +0900

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Staff;
+use App\Photo;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/create', function(){
+    $staff = Staff::find(1);
+    $staff->photos()->create(['path'=>'example.jpg']);
+});
+
+Route::get('/read', function(){
+    $staff = Staff::findOrFail(1);
+    foreach($staff->photos as $photo){
+        return $photo->path;
+    }
+    
+});
+
+Route::get('/update', function(){
+    $staff = Staff::findOrFail(1);
+    $photo = $staff->photos()->whereId(1)->first();
+    $photo->path = "Update example.jpg";
+    $photo->save();
+});
+
+Route::get('/delete', function(){
+    $staff = Staff::findOrFail(1);
+    $staff->photos()->whereId(1)->delete();
+});
+
+Route::get('/assign', function(){
+    $staff = Staff::findPrFail(1);
+    $photo = Photo::findOrFail(4);
+    $staff->photos()->save($photo);
+});
+
+
+Route::get('un-assign', function(){
+    $staff = Staff::findOrFail(1);
+    $staff->photos()->whereId(4)->update(['imagiable_id'=>'', 'imagiable_type'=>'']);
+});
